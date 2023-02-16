f17d6ed0ae481db313355a58a4a3344b1d8b91ea
Quartelf <thomasfava14@gmail.com>
2023-02-16 16:54:13 -0300

    Terceiro commit

--- main.tf
+++ main.tf
@@ -11,23 +11,40 @@ provider "google" {
     region = var.region
 }
 
-resource "google_compute_instance" "vm_instance" {
-    count = 4
-    name = "thomas-instance" 
-    machine_type = "n1-standard-1"
-    zone = "us-east1-b"
 
+
+resource "google_compute_instance" "vm_instance" {
+    count = 1
+    name = "thomas-instance-${count.index}"
+    machine_type = "n1-standard-1" 
+    zone = "us-east1-b"
+    tags = [ "allow-icmp" ]
+    enable_display = true
     boot_disk {
       initialize_params {
-        image = "debian-cloud/debian-11"
+        image = "windows-cloud/windows-2022"
       }
     }
 
     network_interface {
       network = "thomas-network" # funciona se tirar mas tudo bem
-      subnetwork = "subnet-${count.index}.0"
+      subnetwork = google_compute_subnetwork.google_subnets[count.index].name
       access_config {
           
       }
     }
+}
+
+resource "google_compute_firewall" "rule-allow-icmp" {
+  name    = "fw-thomas"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports    = ["3389"]
+  }
+  allow {
+    protocol = "icmp"
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["allow-icmp"]
 }
--- network.tf
+++ network.tf
@@ -7,8 +7,8 @@ resource "google_compute_network" "vpc_network" {
 }
 
 resource "google_compute_subnetwork" "google_subnets" {
-  count         = 4
-  name          = "subnet-${count.index}.0"
+  count         = 1
+  name          = "subnet-${count.index}-0"
   ip_cidr_range = "10.0.${count.index}.0/24"
   region        = var.region
   network       = google_compute_network.vpc_network.id
