4ca67d13e89043d9f7444505ea7099c81f3f1729
Quartelf <thomasfava14@gmail.com>
2023-02-24 16:18:37 -0300

    Changes

--- main.tf
+++ main.tf
@@ -7,44 +7,32 @@ terraform {
 }
 
 provider "google" {
-    project = "malwee-iac"
-    region = var.region
+  project = "malwee-iac"
+  region  = var.region
 }
 
 
 
 resource "google_compute_instance" "vm_instance" {
-    count = 1
-    name = "thomas-instance-${count.index}"
-    machine_type = "n1-standard-1" 
-    zone = "us-east1-b"
-    tags = [ "allow-icmp" ]
-    enable_display = true
-    boot_disk {
-      initialize_params {
-        image = "windows-cloud/windows-2022"
-      }
+  count          = 2
+  name           = "thomas-instance-${count.index}"
+  machine_type   = "n1-standard-1"
+  zone           = "us-east1-b"
+  tags           = ["allow-udp"]
+  enable_display = true
+  boot_disk {
+    initialize_params {
+      image = "windows-cloud/windows-2022"
+      type = "pd-standard"
+      size = 50
     }
+  }
 
-    network_interface {
-      network = "thomas-network" # funciona se tirar mas tudo bem
-      subnetwork = google_compute_subnetwork.google_subnets[count.index].name
-      access_config {
-          
-      }
+  network_interface {
+    network    = "thomas-network" # funciona se tirar mas tudo bem
+    subnetwork = google_compute_subnetwork.google_subnets[count.index].name
+    access_config {
+
     }
-}
-
-resource "google_compute_firewall" "rule-allow-icmp" {
-  name    = "fw-thomas"
-  network = google_compute_network.vpc_network.name
-  allow {
-    protocol = "tcp"
-    ports    = ["3389"]
   }
-  allow {
-    protocol = "icmp"
-  }
-  source_ranges = ["0.0.0.0/0"]
-  target_tags = ["allow-icmp"]
 }
--- network.tf
+++ network.tf
@@ -4,13 +4,32 @@ resource "google_compute_network" "vpc_network" {
   name                    = "thomas-network"
   auto_create_subnetworks = false
   mtu                     = 1460
+  routing_mode =  "REGIONAL"
 }
 
 resource "google_compute_subnetwork" "google_subnets" {
-  count         = 1
+  count         = 2
   name          = "subnet-${count.index}-0"
   ip_cidr_range = "10.0.${count.index}.0/24"
   region        = var.region
   network       = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_firewall" "rule-allow-icmp" {
+  name    = "fw-thomas-rules"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports    = ["3389", "53", "88", "135", "139", "389", "636", "445"]
+  }
+  allow {
+    protocol = "icmp"
+  }
+  allow {
+    protocol = "udp"
+    ports    = ["53", "88", "123", "139", "389", "636"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  source_tags   = ["allow-udp"]
 }
 
